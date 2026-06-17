1e8d71b4f856886c47c5ce79967b9a584301be27
Samuel Costa <samuka.camilo@gmail.com>
2026-06-17 02:27:07 -0300

    fix: path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nutrifit/',
+  base: '/nutrifit',
   plugins: [react()],
   server: {
     port: 5174,
