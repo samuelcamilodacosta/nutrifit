656f02dc2c669bab5c77738aa384d2eb1ab71c6b
Samuel Costa <samuka.camilo@gmail.com>
2026-06-26 19:14:13 -0300

    feat: adicionando build netfily

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.VITE_BASE_PATH || '/nutrifit/'
+
 export default defineConfig({
-  base: '/nutrifit/',
+  base,
   plugins: [react()],
   server: {
     port: 5174,
-    open: '/nutrifit/',
+    open: base,
   },
 })
